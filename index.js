3530bf81cab0c4502379cf8d48c8e7fa88018714
Josh Burd <Jburdt@gmail.com>
2022-06-09 18:52:36 -0400

    commit tree cb976eda0fafda9ba9676e3e932e894f38c4b24d

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+function saturdayFun (activity = 'roller-skate') {
+    if (saturdayFun){
+        return `This Saturday, I want to ${activity}!`
+    }
+} 
+
+function mondayWork(activity = 'go to the office') {
+    if (mondayWork){
+        return `This Monday, I will ${activity}.`
+    }
+}
+
+function wrapAdjective(wrapper = '*') {
+    return function innerFunction(des = 'special') { 
+        return `You are ${wrapper}${des}${wrapper}!`
+    } 
+}
+
+//function called wrapAdjective
+//innerFunction parameter description "special"
+//return You are a dedicated programmer
+
